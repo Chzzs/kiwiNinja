4444b630aabe976fa2ab1256a7249bc9be0e6ae4
Thuranos <rom.larroque@gmail.com>
2015-10-21 17:35:08 +0200

    Lot of things
    Login check
    User list display
    Redirection

--- step1/login/js/controllers/LoginController.js
+++ step1/login/js/controllers/LoginController.js
@@ -1,10 +1,29 @@
 angular.module('loginApp').controller('loginCtrl',loginCrtFnt); 
 
-loginCrtFnt.$inject=['$scope','$log']; 
+loginCrtFnt.$inject=['$scope','$log', 'auth', '$window']; 
 
-function loginCrtFnt($scope, $log){ 
+function loginCrtFnt($scope, $log, auth, $window){ 
+	
 	$scope.logAuth = function() { 
 		$log.info('user login', $scope.user.login); 
 		$log.info('user pwd', $scope.user.pwd); 
 	}; 
+	
+	$scope.checkUser = function(user)
+	{
+		if(auth.checkUser(user.login, user.pwd))
+		{
+			$log.info('user login', user.login); 
+			$log.info('user pwd', user.pwd); 
+			$window.location.href = "loginSuccess.html";
+		}
+		else
+			$log.info('wrong combination login/pwd');
+	}
+	
+	$scope.showUserList = function() {
+		auth.userList().forEach(function(element) {
+			$log.info(element);
+		}, this);
+	}
 }
---
+++ step1/login/js/services/AuthService.js
@@ -0,0 +1,26 @@
+angular.module('authService', []).service('auth',authFnc);
+
+function authFnc() {
+	var userMap={};
+	userMap['jdoe']='jdoepwd';
+	userMap['psmith']='psmithpwd';
+	userMap['tp']='tp';
+	
+	var fncContainer={
+		checkUser: checkUser,
+		userList: userList
+	};
+	
+	function checkUser(userlogin,userpwd){
+		if(userMap[userlogin]==userpwd)
+			return true;
+		
+		return false;
+	};
+	
+	function userList(){
+		return Object.keys(userMap);
+	};
+	
+	return fncContainer;
+}
