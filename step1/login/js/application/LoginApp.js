2fb5f9dc6e6c387dfd427e56386d05242ea8bdc1
vouillon thibaud <vouillon.thibaud@gmail.com>
2015-10-21 15:21:47 +0200

    it works

--- step1/login/js/application/LoginApp.js
+++ step1/login/js/application/LoginApp.js
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-//Creation of an application not needed to bind it to a global variable
+
 angular.module('loginApp', []);
-=======
-angular.module('loginApp', []);
->>>>>>> fbede8050cd1bddd477d08df4b01853feef5bdff
-=======
-//Creation of an application not needed to bind it to a global variable 
-angular.module('loginApp', []);
->>>>>>> 20cfd52d37e357c8b59f59a13e6486be505233cb
